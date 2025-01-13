5af156e75df281038ca0394f6ef1ab8382fd3314
ferdinand <853712064@qq.com>
2025-01-13 15:43:24 +0800

    feat：移植CarEvsCameraActivity模块，加入Evs功能
    1.导入android.car.jar包
    2.将源码中的CarEvsCameraActivity代码移植到App上

--- app/src/main/java/com/ferdinand/reversevision/MainActivity.java
+++ app/src/main/java/com/ferdinand/reversevision/MainActivity.java
@@ -3,19 +3,48 @@ package com.ferdinand.reversevision;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.car.Car;
+import android.car.Car.CarServiceLifecycleListener;
+import android.car.evs.CarEvsManager;
+import static android.car.evs.CarEvsManager.ERROR_NONE;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private AuxiliaryLineView auxiliaryLineView;
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int CAR_WAIT_TIMEOUT_MS = 3_000;
+    private Car mCar;
+
+    /** CarService status listener  */
+    private final CarServiceLifecycleListener mCarServiceLifecycleListener = (car, ready) -> {
+        if (!ready) {
+            return;
+        }
+
+        try {
+            CarEvsManager evsManager = (CarEvsManager) car.getCarManager(
+                    Car.CAR_EVS_SERVICE);
+            if (evsManager.startActivity(CarEvsManager.SERVICE_TYPE_REARVIEW) != ERROR_NONE) {
+                Log.e(TAG, "Failed to start a camera preview activity");
+            }
+        } finally {
+            mCar = car;
+            finish();
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Car.createCar(getApplicationContext(), /* handler = */ null, CAR_WAIT_TIMEOUT_MS,
+                mCarServiceLifecycleListener);
 
         auxiliaryLineView = findViewById(R.id.auxiliaryLineView);
 
@@ -51,6 +80,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        if (mCar != null) {
+            // Explicitly stops monitoring the car service's status
+            mCar.disconnect();
+        }
+
+        super.onDestroy();
     }
 
     private AuxiliaryLineView.GuideLine createTopviewGuideLine() {
