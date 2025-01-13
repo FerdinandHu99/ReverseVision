94fee69ca3f26c0b215259d43a74aed97b918824
ferdinand <853712064@qq.com>
2025-01-13 10:29:08 +0800

    refactor：删除CameraX方案代码

--- app/src/main/java/com/ferdinand/reversevision/MainActivity.java
+++ app/src/main/java/com/ferdinand/reversevision/MainActivity.java
@@ -1,56 +1,21 @@
 package com.ferdinand.reversevision;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ListenableFuture<ProcessCameraProvider> processCameraProviderListenableFuture;
-    private PreviewView previewView;
-    private CameraSelector cameraSelector;
-    private String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA};
-    private static final int REQUEST_CODE_PERMISSIONS = 10;
-
     private AuxiliaryLineView auxiliaryLineView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-            String[] cameraList = cameraManager.getCameraIdList();
-            if (cameraList.length == 0) {
-                Log.e("Camera", "No camera detected on this device.");
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-
-//        previewView = findViewById(R.id.previewView);
-
-        if (allPermissionsGranted()) {
-//            startCamera();
-        } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
-        }
 
         auxiliaryLineView = findViewById(R.id.auxiliaryLineView);
 
@@ -86,50 +51,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-    }
-
-    private boolean allPermissionsGranted() {
-        for (String permission : REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(this, permission)
-                    != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera();
-            }
-        }
-    }
-    private void startCamera() {
-        processCameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
-        processCameraProviderListenableFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider processCameraProvider = processCameraProviderListenableFuture.get();
-                bindCamera(processCameraProvider);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }, ContextCompat.getMainExecutor(this));
-    }
-
-    private void bindCamera(ProcessCameraProvider processCameraProvider) {
-        processCameraProvider.unbindAll();
-        cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                .build();
-
-        Preview preview = new Preview.Builder().build();
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-
-        processCameraProvider.bindToLifecycle(this, cameraSelector, preview);
     }
 
     private AuxiliaryLineView.GuideLine createTopviewGuideLine() {
